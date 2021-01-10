3c2d1cd1126bce6091ff139e6bfebfaefc17a173
Adnormalize <Adnormalize@gmail.com>
2021-01-10 13:27:39 +0700

    First commit

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    private bool _isGrounded;
+    private Rigidbody _rigidBody;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovementLogic();
+        JumpLogic();
+    }
+
+    private void MovementLogic()
+    {
+        float _moveHorizontal = Input.GetAxis("Horizontal");
+        float _moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(_moveHorizontal, 0.0f, _moveVertical);
+
+        _rigidBody.AddForce(_movement * _speed);
+    }
+
+    private void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_isGrounded)
+            {
+                _rigidBody.AddForce(Vector3.up * _jumpForce);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpate(collision, true);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpate(collision, false);
+    }
+
+    private void IsGroundedUpate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            _isGrounded = value;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+
+    [SerializeField] private Transform _position;
+
+    void Start()
+    {
+        Instantiate(_enemy, _position.position, Quaternion.identity);
+    }
+}
